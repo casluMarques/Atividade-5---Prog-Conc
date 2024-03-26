47011ab15a55f3a596b34f9a0a313047b2da5a4d
casluMarques <131936376+casluMarques@users.noreply.github.com>
2024-03-26 10:35:22 -0300

    Update pc-5-rand-v1.c

--- pc-5-rand-v1.c
+++ pc-5-rand-v1.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <time.h>
 
-
-
+#define M 8
 
 //criando a struct que armazena os argumentos a serem utilizados pela thread
 typedef struct
 {
-    int *end_vetor, indice;
+    int *end_vetor, indice, tamanho_vetor;
 } t_Args;
 
 //função a ser executada pelas Threads
 void *QuadradoVetor (void *arg){
 	t_Args args = *(t_Args*) arg;
-	for (i=args.indice; i<args.tamanho_vetor; i+M ){
+    for (int i=args.indice; i<args.tamanho_vetor; i+M ){
         args.end_vetor = args.end_vetor+args.indice;
         *args.end_vetor = *args.end_vetor * *args.end_vetor;
     }
+	//printf("Vetor na posição %d, com valor atualizado %d\n", args.indice, *args.end_vetor);
     pthread_exit(NULL);
 }
 
@@ -26,7 +25,7 @@ void *QuadradoVetor (void *arg){
 //fluxo principal do programa
 
 int main (int argc, char* argv[]){
-    int nthreads; /*número de threads, passado como argumento na
+    int nthreads; /*tamanho do vetor, passado como argumento na
     chamada do executável */
     if(argc<2){  //verificando se foram passados os parâmetros
         printf("--ERRO: A quantidade de threads não foi informada, <%s><nthreads>\n", argv[0]);
@@ -35,8 +34,7 @@ int main (int argc, char* argv[]){
     //passando so argumentos inseridos por linha de comando para int
     nthreads = atoi(argv[1]);  
 
-
-    printf("Vetor a ser atualizado:\n");
+	printf("Vetor a ser atualizado:\n");
     srand(time(NULL));//inicializando o rand
 	int vec[nthreads];//vetor a ser inicializado
 	for(int i =0; i < nthreads; i++ ){ //loop para inicialização dos valores do vetor
@@ -53,7 +51,7 @@ int main (int argc, char* argv[]){
     t_Args *args; //criando ponteiro para a variável que receberá os argumentos das threads 
     //criando as threads, por um loop for
 
-    for(int i =0; i < nthreads; i++ ){ //loop para criação e incialização das threads
+    for(int i =0; i < M; i++ ){ //loop para criação e incialização das threads
         printf("--Alocando e preenchendo argumentos para a thread %d\n", i);
         args = malloc(sizeof(t_Args));
         if (args == NULL){
@@ -62,6 +60,7 @@ int main (int argc, char* argv[]){
 		//int val = vec[i];
         args->end_vetor = &vec;
 		args->indice = i;
+        args->tamanho_vetor = nthreads;
         printf("##Criando thread %d\n", i);
         if(pthread_create(&id_tds_sys[i], NULL, QuadradoVetor, (void *) args)){
             printf("Erro na criação da thread %d\n", i);
